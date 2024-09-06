b8f47d3dd76260abaadb044d4f8533f2a02d121f
Sei Yeu Chew <20chewseiyeu@gmail.com>
2024-09-06 09:05:12 +0100

    upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+  
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState([])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo,todoIndex) => {
+      return todoIndex !== index
+    })
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+    const valueTobBeEdited = todos[index]
+    setTodoValue(valueTobBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  return (
+    <>
+       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+       <TodoList handleDeleteTodo={handleDeleteTodo} handleEditTodo={handleEditTodo} todos={todos}/>
+    </>
+  )
+}
+
+export default App
